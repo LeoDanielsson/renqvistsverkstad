515a324eb879e9bfbecbe27e894bd7c31de7b592
leo <leo.danielsson@tele2.com>
2020-04-05 16:32:53 +0200

    darken white color

--- colors.js
+++ colors.js
@@ -1,5 +1,5 @@
 export const gold = '#a8763e';
 export const linen = '#f7f3e3';
-export const white = '#ecf0f1';
+export const white = '#e6eaeb';
 export const copper = '#6f1a07';
 export const black = '#03191e';
