ef5133c1b5f74b95a60404d681336032f07cecb8
leo <leo.danielsson@tele2.com>
2020-04-29 21:42:02 +0200

    greyer white

--- colors.js
+++ colors.js
@@ -1,6 +1,6 @@
 export const gold = '#a8763e';
 export const linen = '#f7f3e3';
-export const white = '#ecf0f1';
+export const white = '#d9ddde';
 export const copper = '#6f1a07';
 export const darkGreen = '#03191e';
 export const black = '#333';
