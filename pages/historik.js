35ca44df6d58c1eedce4b0b0c6e5db4f6f2fd586
Leo Danielsson <leo.danielsson@syson.se>
2024-10-08 07:20:31 +0200

    fix history page sorting

--- pages/historik.js
+++ pages/historik.js
@@ -16,7 +16,7 @@ export async function getExhibition(filename) {
 export async function getStaticProps({ params }) {
   const filenames = fs.readdirSync("content/exhibitions");
   const exhibitions = await Promise.all(filenames.map(getExhibition));
-
+  console.log(exhibitions);
   return {
     props: {
       exhibitions,
@@ -28,16 +28,23 @@ const ExhibitionHistory = ({ exhibitions }) => (
   <Layout>
     <Container>
       <h1>Historik</h1>
-      {exhibitions.slice(1).map(({ slug, attributes: { title, sections } }) => (
-        <Section
-          key={slug}
-          title={title}
-          image={sections[0].image}
-          link={`/utstallningar/${slug}`}
-        >
-          <ReactMarkdown>{sections[0].body}</ReactMarkdown>
-        </Section>
-      ))}
+      {exhibitions
+        .sort((a, b) => {
+          const dateA = new Date(a.attributes.date);
+          const dateB = new Date(b.attributes.date);
+          return dateB - dateA;
+        })
+        .slice(1)
+        .map(({ slug, attributes: { title, sections } }) => (
+          <Section
+            key={slug}
+            title={title}
+            image={sections[0].image}
+            link={`/utstallningar/${slug}`}
+          >
+            <ReactMarkdown>{sections[0].body}</ReactMarkdown>
+          </Section>
+        ))}
     </Container>
   </Layout>
 );
